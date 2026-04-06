49c6c075ac93eb78739ada18020307910744d2d0
carlos123-hub <carlosdanielcondoriparizaca4@gmail.com>
2026-04-05 20:19:37 -0500

    Update Main.java

--- Nivel1/Ejercicio1/Main.java
+++ Nivel1/Ejercicio1/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Persona p = new Persona("Carlos", 20, "correo@gmail.com");
-        p.presentarse();
+        Persona p1 = new Persona("Carlos", 20, "carlos@gmail.com");
+        Persona p2 = new Persona("Ana", 19, "ana@gmail.com");
+        p1.presentarse();
+        p2.presentarse();
     }
 }
