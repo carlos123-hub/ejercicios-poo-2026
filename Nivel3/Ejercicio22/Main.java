5665653842f174d82c3de9dbbef1aec8be309448
carlos123-hub <carlosdanielcondoriparizaca4@gmail.com>
2026-04-05 21:24:01 -0500

    Create Main.java

---
+++ Nivel3/Ejercicio22/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Perro p = new Perro();
+        p.respirar();
+        p.moverse();
+        p.amamantar();
+        p.ladrar();
+    }
+}
