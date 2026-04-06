d29fdad995907f7921703627ae271d2637234c10
carlos123-hub <carlosdanielcondoriparizaca4@gmail.com>
2026-04-05 21:21:57 -0500

    Create Main.java

---
+++ Nivel3/Ejercicio21/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ListaLimitada l = new ListaLimitada(2);
+        l.agregar("A");
+        l.agregar("B");
+        l.listar();
+    }
